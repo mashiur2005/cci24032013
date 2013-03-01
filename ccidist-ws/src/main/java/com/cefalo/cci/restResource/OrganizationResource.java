256050265cf0264ccee663bc8dc0a684ea471df3
Partha Guha Roy <partha@cefalo.no>
2013-03-01 09:20:50 +0600

    CCIDIST-28: Fixed 304 code for organization resource.

--- ccidist-ws/src/main/java/com/cefalo/cci/restResource/OrganizationResource.java
+++ ccidist-ws/src/main/java/com/cefalo/cci/restResource/OrganizationResource.java
@@ -1,5 +1,24 @@
 package com.cefalo.cci.restResource;
 
+import java.net.URI;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
+
 import com.cefalo.cci.mapping.JerseyResourceLocator;
 import com.cefalo.cci.mapping.ResourceLocator;
 import com.cefalo.cci.model.Organization;
@@ -11,19 +30,6 @@ import com.sun.jersey.api.NotFoundException;
 import com.sun.jersey.api.Responses;
 import com.sun.jersey.api.view.Viewable;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.UriInfo;
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 @Path("/")
 public class OrganizationResource {
     @Inject
@@ -31,6 +37,9 @@ public class OrganizationResource {
 
     @Context
     private UriInfo uriInfo;
+
+    @Context
+    private Request request;
 
     @GET
     @Produces(MediaType.APPLICATION_XHTML_XML)
@@ -58,36 +67,34 @@ public class OrganizationResource {
     @GET
     @Path("/{organization}")
     @Produces(MediaType.APPLICATION_XHTML_XML)
-    public Response getOrganizationDetail(@PathParam("organization") final String organization, @HeaderParam("If-None-Match") @DefaultValue("-1") final long ifNoneMatchVersion) {
-        if (Utils.isBlank(organization)) {
+    public Response getOrganizationDetail(@PathParam("organization") final String organizationId) {
+        if (Utils.isBlank(organizationId)) {
             return Responses.clientError().entity("Organization name may not be empty").build();
         }
-        Organization org = organizationService.getOrganization(organization.toLowerCase());
-        if (org == null) {
-            throw  new NotFoundException();
+
+        Organization organization = organizationService.getOrganization(organizationId);
+        if (organization == null) {
+            throw new NotFoundException();
         }
 
-        if (org.getVersion() == ifNoneMatchVersion) {
-            return Response.notModified().build();
+        ResponseBuilder unmodifiedResponseBuilder = request.evaluatePreconditions(EntityTag.valueOf(Utils
+                .createETagHeaderValue(organization.getVersion())));
+        if (unmodifiedResponseBuilder != null) {
+            return unmodifiedResponseBuilder.build();
         }
 
         ResourceLocator resourceLocator = JerseyResourceLocator.from(uriInfo);
-        Map<String, URI> publicationNameUriMap = new HashMap<String, URI>();
+        Map<String, URI> publicationNameUriMap = new TreeMap<>();
 
-        Iterator<Publication> publicationIterator = org.getPublications().iterator();
-
-        while (publicationIterator.hasNext()) {
-            Publication tempPublication = publicationIterator.next();
-            publicationNameUriMap.put(tempPublication.getName(), resourceLocator.getPublicationURI(org.getId(), tempPublication.getId()));
+        for (Publication publication : organization.getPublications()) {
+            publicationNameUriMap.put(publication.getName(),
+                    resourceLocator.getPublicationURI(organization.getId(), publication.getId()));
         }
 
-
-        Map<String, Object> model = new HashMap<String, Object>();
-        model.put("organizationName", org.getName());
+        Map<String, Object> model = new HashMap<>();
+        model.put("organizationName", organization.getName());
         model.put("publicationMap", publicationNameUriMap);
 
-        ResponseBuilder responseBuilder = Response.ok(new Viewable("/organization", model));
-        responseBuilder = responseBuilder.tag(String.valueOf(org.getVersion()));
-        return responseBuilder.build();
+        return Response.ok(new Viewable("/organization", model)).tag(String.valueOf(organization.getVersion())).build();
     }
 }
--- ccidist-ws/src/main/webapp/WEB-INF/jsp/organization.jsp
+++ ccidist-ws/src/main/webapp/WEB-INF/jsp/organization.jsp
@@ -1,17 +1,19 @@
-<%@ page contentType="application/xhtml+xml;charset=UTF-8" language="java" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ page contentType="application/xhtml+xml;charset=UTF-8" language="java"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head><title>Organization</title></head>
+<head>
+<title>Organization</title>
+</head>
 <body>
-<h1>Organization</h1>
-<h3>${it.organizationName}</h3>
-<ul class="organization">
-    <c:forEach items="${it.publicationMap}" var="publicationMap">
-        <li><a href="${publicationMap.value}">${publicationMap.key}</a></li>
-    </c:forEach>
-</ul>
+    <h1>Organization</h1>
+    <h3>${it.organizationName}</h3>
+    <ul class="organization">
+        <c:forEach items="${it.publicationMap}" var="publicationMap">
+            <li><a href="<c:out value="${publicationMap.value}"/>"><c:out value="${publicationMap.key}" /></a></li>
+        </c:forEach>
+    </ul>
 </body>
 </html>
