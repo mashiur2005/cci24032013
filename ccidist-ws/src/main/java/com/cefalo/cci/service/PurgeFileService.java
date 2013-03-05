18cd7ba968f787fe04357ed544d510a5f3282b76
Mashiur Rahman <mashiur@cefalo.no>
2013-03-05 18:05:50 +0600

    CCIDIST-30: Joda Used for calculating the date

--- ccidist-ws/src/main/java/com/cefalo/cci/service/PurgeFileService.java
+++ ccidist-ws/src/main/java/com/cefalo/cci/service/PurgeFileService.java
@@ -3,13 +3,13 @@ package com.cefalo.cci.service;
 import com.cefalo.cci.model.Issue;
 import com.cefalo.cci.utils.Utils;
 import com.google.inject.Inject;
+import org.joda.time.DateTime;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -28,9 +28,8 @@ public class PurgeFileService implements org.quartz.Job{
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         logger.info("cron job working for removing old files ......." + System.currentTimeMillis());
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, daysBack * -1);
-        long purgeTime = cal.getTimeInMillis();
+        DateTime prevDate = new DateTime();
+        long purgeTime = prevDate.minusDays(daysBack).getMillis();
         Date purgeDate = new Date(purgeTime);
         List<Issue> issueList = issueService.getOldIssueList(purgeDate);
 
